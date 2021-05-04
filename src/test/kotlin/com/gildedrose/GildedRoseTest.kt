862858504935d816e62058b3d31dcc69f5c0ebdd
Patrick Lamprecht <lamprecht.patrickt1@gmail.com>
2021-05-04 18:54:04 +0200

    IDE hints and formatting

--- src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -7,13 +7,9 @@ internal class GildedRoseTest {
 
     @Test
     fun foo() {
-        val items = arrayOf<Item>(Item("foo", 0, 0))
+        val items = arrayOf(Item("foo", 0, 0))
         val app = GildedRose(items)
         app.updateQuality()
         assertEquals("fixme", app.items[0].name)
-
     }
-
 }
-
-
