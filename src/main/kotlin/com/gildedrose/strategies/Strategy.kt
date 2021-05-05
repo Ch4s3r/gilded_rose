d78d3b086e9ec3d1904784edcbd785b46071e79f
Patrick Lamprecht <lamprecht.patrickt1@gmail.com>
2021-05-05 10:00:09 +0200

    Add new line

--- src/main/kotlin/com/gildedrose/strategies/Strategy.kt
+++ src/main/kotlin/com/gildedrose/strategies/Strategy.kt
@@ -3,6 +3,7 @@ package com.gildedrose.strategies
 import com.gildedrose.Product
 
 interface TestableUpdateStrategy : UpdateStrategy, Testable
+
 interface UpdateStrategy {
     fun update(product: Product): Product
 }
